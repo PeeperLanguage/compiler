4f3c4f6055242ffe90b5738d0fb219b0fe639ccf
Fuad Hasan <fuad.cs22@gmail.com>
2026-07-11 19:14:42 +0600

    Consolidate compiler problems
    Keep the small shared problem-reporting package in one file instead of splitting two diagnostic functions by error kind.

--- internal/problems/problems.go
+++ internal/problems/problems.go
@@ -1,11 +1,22 @@
 package problems
 
 import (
+	"fmt"
+
 	"compiler/internal/diagnostics"
 	"compiler/internal/project"
 	"compiler/internal/semantics/table"
 	"compiler/internal/source"
 )
+
+func ArrayIndexOutOfBounds(index, length string, loc *source.Location) *diagnostics.Diagnostic {
+	msg := fmt.Sprintf("array index out of bounds: index %s for length %s", index, length)
+	d := diagnostics.NewError(msg).WithCode(diagnostics.ErrArrayOutOfBounds)
+	if loc != nil {
+		d.WithPrimaryLabel(loc, msg)
+	}
+	return d
+}
 
 func ReportRedeclaration(ctx *project.CompilerContext, scope *table.Scope, err string, name string, loc *source.Location) {
 	if ctx == nil || ctx.Diagnostics == nil {
